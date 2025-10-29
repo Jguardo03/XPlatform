77cfffc70806bb8639fb81aa76dec86bd0ab5666
Dean <14750@ait.nsw.edu.au>
2025-10-29 17:57:30 +1100

    Recommit to share firebase config

---
+++ config/config.ts
@@ -0,0 +1,9 @@
+// Add to git ignore when everyone has it working
+export const firebaseConfig = {
+  apiKey: "AIzaSyC9jh35jC9GlIzMmWwmYv13plNbLPLOY40",
+  authDomain: "gameseerr-x.firebaseapp.com",
+  projectId: "gameseerr-x",
+  storageBucket: "gameseerr-x.firebasestorage.app",
+  messagingSenderId: "239924915077",
+  appId: "1:239924915077:web:a94d87c4b1827b9e1632ec"
+};
